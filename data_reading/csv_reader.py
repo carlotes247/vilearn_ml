b227a0156060289067702b738c9dfe4d5b7c4692
cristinadobre <33484575+cristinadobre@users.noreply.github.com>
2024-04-09 14:22:44 +0200

    Update csv_reader.py

--- data_reading/csv_reader.py
+++ data_reading/csv_reader.py
@@ -20,13 +20,16 @@ class csv_reader:
     imuTS = []
     eyeTS = []
     # device status
-    headsetValid, leftHandValid, rightHandValid, isUserPresent = []
+    headsetValid = []
+    leftHandValid = []
+    rightHandValid= []
+    isUserPresent = []
     # head
     headAcc = []
     headGyro = []
     # head transform
     headPosition = []
-    headRotation =  []
+    headRotation = []
     # left hand transform
     leftHandPosition = []
     leftHandRotation = []
@@ -55,7 +58,7 @@ class csv_reader:
 
     #region Constructor
     def __init__(self, path):
-        self.raw_data = pd.read_csv(path)
+        self.raw_data = pd.read_csv(path, decimal=',', sep=';')
         # timestamp   
         self.overallTsString = self.raw_data['overallTS']
         self.overallTsNtpString = self.raw_data['overallTsNTP']
@@ -120,19 +123,19 @@ class csv_reader:
             # Loop through each row in the file
             for row in reader:
                 # Get the first column as timestamp
-                timestamp = row[0]   
+                timestamp = row[0]
                 # Parse the timestamp string into a datetime object
-                dt = datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S.%f")                
+                dt = datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S.%f")
                 if (not firstInit):
                     prevTimestamp = listOfTimeStamps[-1]
                     timestamptRepeated = timestamp == prevTimestamp
                     #print("dtDiff is: " + str(timestamptRepeated))
-                    
+
                 # Avoid duplicate timestamps
-                if (firstInit or not timestamptRepeated):                  
+                if (firstInit or not timestamptRepeated):
                     # Print the timestamp
                     #print(timestamp)
-                    # Add timestamp into list 
+                    # Add timestamp into list
                     listOfTimeStamps.append(timestamp)
                     # Get the microseconds part
                     microseconds = dt.microsecond
@@ -148,105 +151,105 @@ class csv_reader:
                         listOfDeltas.append(delta)
                     previousMiliseconds = currentMiliseconds
                     firstInit = False
-                
+
         return listOfDeltas
 
     def getEyeTrackingData(self, path):
         """
         (Uses Pandas module) Returns a list of eye tracking
         of a csv file where the first column are the timestamps
-        """ 
+        """
         # IMU data
         print("--------------------- IMU -----------------------------------")
-        # timestamp      
-        data = pd.read_csv(path)
-        print(f"Percentage of NaN values in IMU TimeStamp: {data['imuTS'].isna().mean()*100}%")
-        print(f"Percentage of NaN values in EYE TimeStamp: {data['eyeTS'].isna().mean()*100}%")
+        # timestamp
+        data = pd.read_csv(path, decimal=',', sep=';')
+        print(f"Percentage of NaN values in IMU TimeStamp: {self.imuTS.isna().mean()*100}%")
+        print(f"Percentage of NaN values in EYE TimeStamp: {self.eyeTS.isna().mean()*100}%")
         print("-------------------------------------------------------------")
-        print(f"Percentage of False values in Head isValid: {data['headsetValid'].isin([False]).mean()*100}%")
-        print(f"Percentage of False values in leftHandValid: {data['leftHandValid'].isin([False]).mean()*100}%")
-        print(f"Percentage of False values in rightHandValid: {data['rightHandValid'].isin([False]).mean()*100}%")
-        print(f"Percentage of False values in isUserPresent: {data['isUserPresent'].isin([False]).mean()*100}%")        
+        print(f"Percentage of False values in Head isValid: {self.headsetValid.isin([False]).mean()*100}%")
+        print(f"Percentage of False values in leftHandValid: {self.leftHandValid.isin([False]).mean()*100}%")
+        print(f"Percentage of False values in rightHandValid: {self.rightHandValid.isin([False]).mean()*100}%")
+        print(f"Percentage of False values in isUserPresent: {self.isUserPresent.isin([False]).mean()*100}%")
         print("-------------------------------------------------------------")
         # headAcc
-        headAcc = data[["headAccX", "headAccY", "headAccZ"]]
-        print(f"Percentage of -Infinity values in:\n{headAcc.isin([-np.inf]).mean()*100}")        
+        # headAcc = data[["headAccX", "headAccY", "headAccZ"]]
+        print(f"Percentage of -Infinity values in:\n{self.headAcc.isin([-np.inf]).mean()*100}")
         print("-------------------------------------------------------------")
         # headGyro
-        headGyro = data[["headGyroX", "headGyroY", "headGyroZ"]]
-        print(f"Percentage of -Infinity values in:\n{headGyro.isin([-np.inf]).mean()*100}")        
+        # headGyro = data[["headGyroX", "headGyroY", "headGyroZ"]]
+        print(f"Percentage of -Infinity values in:\n{self.headGyro.isin([-np.inf]).mean()*100}")
         print("-------------------------------------------------------------")
-        
+
         # leftEye
         print("----------------- LEFT EYE ----------------------------------")
         # Gaze
-        leftEyeGaze = data[["leftEyeGazeX", "leftEyeGazeY", "leftEyeGazeZ", "leftEyeGazeConfidence"]]
-        print(f"Percentage of NaN values in:\n{leftEyeGaze.isna().mean()*100}")        
+        # leftEyeGaze = data[["leftEyeGazeX", "leftEyeGazeY", "leftEyeGazeZ", "leftEyeGazeConfidence"]]
+        print(f"Percentage of NaN values in:\n{self.leftEyeGaze.isna().mean()*100}")
         print("-------------------------------------------------------------")
         # PupilPosition_Position
-        leftPupilPosition = data[["leftEyePupilPosition_PositionX", "leftEyePupilPosition_PositionY", "leftEyePupilPosition_Position_Confidence"]]
-        print(f"Percentage of NaN values in:\n{leftPupilPosition.isna().mean()*100}")  
-        print(f"Percentage of 0 Confidence entries on Left Pupil Position: {(data['leftEyePupilPosition_Position_Confidence'] == 0).mean()*100}%")    
+        # leftPupilPosition = data[["leftEyePupilPosition_PositionX", "leftEyePupilPosition_PositionY", "leftEyePupilPosition_Position_Confidence"]]
+        print(f"Percentage of NaN values in:\n{self.leftPupilPosition.isna().mean()*100}")
+        print(f"Percentage of 0 Confidence entries on Left Pupil Position: {(data['leftEyePupilPosition_Position_Confidence'] == 0).mean()*100}%")
         print("-------------------------------------------------------------")
         # PupilDilation
-        leftPupilDilation = data[["leftEyePupilDilation", "leftEyePupilDilationConfidence"]]
-        print(f"Percentage of NaN values in:\n{leftPupilDilation.isna().mean()*100}")  
-        print(f"Percentage of 0 Confidence entries on Left Pupil Dilation: {(data['leftEyePupilDilationConfidence'] == 0).mean()*100}%")    
+        # leftPupilDilation = data[["leftEyePupilDilation", "leftEyePupilDilationConfidence"]]
+        print(f"Percentage of NaN values in:\n{self.leftPupilDilation.isna().mean()*100}")
+        print(f"Percentage of 0 Confidence entries on Left Pupil Dilation: {(data['leftEyePupilDilationConfidence'] == 0).mean()*100}%")
         print("-------------------------------------------------------------")
         # EyeOpeness
-        leftEyeOpeness = data[["leftEyeOpeness", "leftEyeOpenessConfidence"]]
-        print(f"Percentage of NaN values in:\n{leftEyeOpeness.isna().mean()*100}")  
+        # leftEyeOpeness = data[["leftEyeOpeness", "leftEyeOpenessConfidence"]]
+        print(f"Percentage of NaN values in:\n{self.leftEyeOpeness.isna().mean()*100}")
         print(f"Percentage of 0 Confidence entries on Left Eye Openess: {(data['leftEyeOpenessConfidence'] == 0).mean()*100}%")
         print("-------------------------------------------------------------")
-        
+
         # rightEye
         print("----------------- RIGHT EYE ---------------------------------")
         # Gaze
-        rightEyeGaze = data[["rightEyeGazeX", "rightEyeGazeY", "rightEyeGazeZ", "rightEyeGazeConfidence"]]
-        print(f"Percentage of NaN values in:\n{rightEyeGaze.isna().mean()*100}")        
+        # rightEyeGaze = data[["rightEyeGazeX", "rightEyeGazeY", "rightEyeGazeZ", "rightEyeGazeConfidence"]]
+        print(f"Percentage of NaN values in:\n{self.rightEyeGaze.isna().mean()*100}")
         print("-------------------------------------------------------------")
         # PupilPosition_Position
-        rightPupilPosition = data[["rightEyePupilPosition_PositionX", "rightEyePupilPosition_PositionY", "rightEyePupilPosition_Position_Confidence"]]
-        print(f"Percentage of NaN values in:\n{rightPupilPosition.isna().mean()*100}")  
-        print(f"Percentage of 0 Confidence entries on right Pupil Position: {(data['rightEyePupilPosition_Position_Confidence'] == 0).mean()*100}%")    
+        # rightPupilPosition = data[["rightEyePupilPosition_PositionX", "rightEyePupilPosition_PositionY", "rightEyePupilPosition_Position_Confidence"]]
+        print(f"Percentage of NaN values in:\n{self.rightPupilPosition.isna().mean()*100}")
+        print(f"Percentage of 0 Confidence entries on right Pupil Position: {(data['rightEyePupilPosition_Position_Confidence'] == 0).mean()*100}%")
         print("-------------------------------------------------------------")
         # PupilDilation
-        rightPupilDilation = data[["rightEyePupilDilation", "rightEyePupilDilationConfidence"]]
-        print(f"Percentage of NaN values in:\n{rightPupilDilation.isna().mean()*100}")  
-        print(f"Percentage of 0 Confidence entries on right Pupil Dilation: {(data['rightEyePupilDilationConfidence'] == 0).mean()*100}%")    
+        # rightPupilDilation = data[["rightEyePupilDilation", "rightEyePupilDilationConfidence"]]
+        print(f"Percentage of NaN values in:\n{self.rightPupilDilation.isna().mean()*100}")
+        print(f"Percentage of 0 Confidence entries on right Pupil Dilation: {(data['rightEyePupilDilationConfidence'] == 0).mean()*100}%")
         print("-------------------------------------------------------------")
         # EyeOpeness
-        rightEyeOpeness = data[["rightEyeOpeness", "rightEyeOpenessConfidence"]]
-        print(f"Percentage of NaN values in:\n{rightEyeOpeness.isna().mean()*100}")  
+        # rightEyeOpeness = data[["rightEyeOpeness", "rightEyeOpenessConfidence"]]
+        print(f"Percentage of NaN values in:\n{self.rightEyeOpeness.isna().mean()*100}")
         print(f"Percentage of 0 Confidence entries on right Eye Openess: {(data['rightEyeOpenessConfidence'] == 0).mean()*100}%")
         print("-------------------------------------------------------------")
-        
+
         # combinedGaze
         print("----------------- COMBINED GAZE -----------------------------")
-        combinedGaze = data[["combinedGazeX", "combinedGazeY", "combinedGazeZ"]]
-        print(f"Percentage of NaN values in:\n{combinedGaze.isna().mean()*100}")  
+        # combinedGaze = data[["combinedGazeX", "combinedGazeY", "combinedGazeZ"]]
+        print(f"Percentage of NaN values in:\n{self.combinedGaze.isna().mean()*100}")
         print("-------------------------------------------------------------")
 
         # leftHand
         print("----------------- LEFT HAND ---------------------------------")
         # HandVelocity
-        leftHandVelocity = data[["leftHandVelocityX", "leftHandVelocityY", "leftHandVelocityZ"]]
-        print(f"Percentage of -Infinity values in:\n{leftHandVelocity.isin([-np.inf]).mean()*100}")        
+        # leftHandVelocity = data[["leftHandVelocityX", "leftHandVelocityY", "leftHandVelocityZ"]]
+        print(f"Percentage of -Infinity values in:\n{self.leftHandVelocity.isin([-np.inf]).mean()*100}")
         print("-------------------------------------------------------------")
         # HandAngularVelocity
-        leftHandAngularVelocity = data[["leftHandAngularVelocityX", "leftHandAngularVelocityY", "leftHandAngularVelocityZ"]]
-        print(f"Percentage of -Infinity values in:\n{leftHandAngularVelocity.isin([-np.inf]).mean()*100}")        
+        # leftHandAngularVelocity = data[["leftHandAngularVelocityX", "leftHandAngularVelocityY", "leftHandAngularVelocityZ"]]
+        print(f"Percentage of -Infinity values in:\n{self.leftHandAngularVelocity.isin([-np.inf]).mean()*100}")
         print("-------------------------------------------------------------")
 
         # rightHand
         print("----------------- RIGHT HAND --------------------------------")
         # HandVelocity
-        rightHandVelocity = data[["rightHandVelocityX", "rightHandVelocityY", "rightHandVelocityZ"]]
-        print(f"Percentage of -Infinity values in:\n{rightHandVelocity.isin([-np.inf]).mean()*100}")        
+        # rightHandVelocity = data[["rightHandVelocityX", "rightHandVelocityY", "rightHandVelocityZ"]]
+        print(f"Percentage of -Infinity values in:\n{self.rightHandVelocity.isin([-np.inf]).mean()*100}")
         print("-------------------------------------------------------------")
         # HandAngularVelocity
-        rightHandAngularVelocity = data[["rightHandAngularVelocityX", "rightHandAngularVelocityY", "rightHandAngularVelocityZ"]]
-        print(f"Percentage of -Infinity values in:\n{rightHandAngularVelocity.isin([-np.inf]).mean()*100}")        
+        # rightHandAngularVelocity = data[["rightHandAngularVelocityX", "rightHandAngularVelocityY", "rightHandAngularVelocityZ"]]
+        print(f"Percentage of -Infinity values in:\n{self.rightHandAngularVelocity.isin([-np.inf]).mean()*100}")
         print("-------------------------------------------------------------")
 
         listOfEyeTracking = []
@@ -255,5 +258,9 @@ class csv_reader:
         return listOfEyeTracking
     #endregion
 
-# reader = csv_reader()
-# reader.getEyeTrackingData("Thomas_HCM153_Administrator2023-10-05__16-13-46.496.csv")
+
+datafilepath='../data/DESKTOP-QTU96C2_vilearn2023-12-19__12-26-14.586.csv'
+datafilepath_mun06NovPC5 = '../data/DESKTOP-QTU96C2_vilearn2023-11-06__16-09-26.660.csv'
+reader = csv_reader(datafilepath_mun06NovPC5)
+eyeTracking = reader.getEyeTrackingData(datafilepath_mun06NovPC5)
+print(eyeTracking)
