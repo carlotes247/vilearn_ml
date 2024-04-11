400ac4d7e1478bbb7477c28d165f31935928a848
Carlos Gonzalez Diaz <carlotes_247@hotmail.com>
2024-04-11 19:36:42 +0200

    skeleton for group and participant data structures

---
+++ data_reading/group.py
@@ -0,0 +1,9 @@
+import participant
+
+class group:
+    """
+    An instance of a group, which can be a dyad or triad 
+    """
+    csv_file_paths = []
+    audio_file_paths = []
+    participants = []
