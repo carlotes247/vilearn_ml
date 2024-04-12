4301c7f48bb3b5509337976d5117819b89983ba3
Carlos Gonzalez Diaz <carlotes_247@hotmail.com>
2024-04-12 22:32:21 +0200

    Set of classes to load group data working

--- data_reading/participant.py
+++ data_reading/participant.py
@@ -1,9 +1,18 @@
-import data_reading.vilearn_csv_data_loader as vilearn_csv_data_loader
+from vilearn_csv_data_loader import *
 
-class participant:
+class Participant:
     """
     An instance of a participant, with its movement and audio data 
     """
     csv_file_path = ""
-    movement_data = vilearn_csv_data_loader(csv_file_path)
+    audio_file_path = ""
+    movement_data: ViLearnCSVDataLoader
     audio_data = ""
+
+    def __init__(self, csv_path: str, audio_path: str):
+        self.csv_file_path = csv_path
+        self.audio_file_path = audio_path
+        self.movement_data = ViLearnCSVDataLoader(self.csv_file_path)
+        self.audio_data = ""
+        # debug loaded data
+        self.movement_data.get_eye_tracking_data()
