96dec152ef5520c73ce2b2ef4469e641ba47aaf2
Carlos Gonzalez Diaz <carlotes_247@hotmail.com>
2024-05-02 18:10:37 +0200

    Created skeleton to import group feature file data

--- data_reading/participant.py
+++ data_reading/participant.py
@@ -6,13 +6,13 @@ class Participant:
     """
     csv_file_path = ""
     audio_file_path = ""
-    movement_data: ViLearnCSVDataLoader
+    movement_data: ViLearnParticipantCSVLoader
     audio_data = ""
 
     def __init__(self, csv_path: str, audio_path: str):
         self.csv_file_path = csv_path
         self.audio_file_path = audio_path
-        self.movement_data = ViLearnCSVDataLoader(self.csv_file_path)
+        self.movement_data = ViLearnParticipantCSVLoader(self.csv_file_path)
         # TODO: create an audio loader (if needed)
         self.audio_data = ""
         # debug loaded data
