0290ee7932206ef635418eccc4be5b137e25ab7e
Carlos Gonzalez Diaz <carlotes_247@hotmail.com>
2024-05-07 12:20:23 +0200

    group feature frames are correctly loaded including the cognition value per frame

--- data_reading/features/group_feature_frame.py
+++ data_reading/features/group_feature_frame.py
@@ -11,11 +11,12 @@ class GroupFeatureFrame:
     participants_features: list[ParticipantFeatures]
     cognition_value: float
 
-    def __init__(self, group_name: str, ts_group_string: str):
+    def __init__(self, group_name: str, ts_group_string: str, cognition: float):
         self.group_name = group_name
         self.ts_group_string = ts_group_string
-        self.ts_group = datetime.datetime.strptime(ts_group_string)
+        self.ts_group = datetime.datetime.strptime(ts_group_string, '%Y-%m-%d %H:%M:%S.%f')
         self.participants_features = []
+        self.cognition_value = cognition
 
     def add_participant_feature(self, participant_feature: ParticipantFeatures):
         self.participants_features.append(participant_feature)
