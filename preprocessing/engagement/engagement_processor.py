b235aaaf0bc38ddd22d6b99d0d6644ea9195b34d
Carlos Gonzalez Diaz <carlotes_247@hotmail.com>
2025-07-26 14:40:51 +0200

    Engagement manager can process all files at 90Hz and 60Hz and produces an average file with TS at the corresponding freq

--- preprocessing/engagement/engagement_processor.py
+++ preprocessing/engagement/engagement_processor.py
@@ -4,34 +4,48 @@ import numpy as np
 class EngagementProcessor:
 
     col_names = ["task_eng", "conf"]
-    path_file_1: str = "data/annotations/dyad_01/group.task engagement.helenrisack.annotation~"
-    path_file_2: str = "data/annotations/dyad_01/task engagement.group.carlosgonzalez.annotation~"
+    data_path: str = "data/annotations"
     path_groups_info: str = "data/group_durations_all_commas.csv"
+    filename_1: str 
+    filename_2: str 
+    path_file_1: str
+    path_file_2: str 
     interaction_start: float = 0
     interaction_end: float = 0
     interaction_length: float = 0
-    group_name: str = "dyad_01"
-    save_df: bool = True
+    freq: int = 0
+    group_name: str
+    save_df: bool = False
     df_eng_1: pd.DataFrame 
     df_eng_2 : pd.DataFrame 
     df_groups_info: pd.DataFrame
 
 
-    def __init__(self, path_groups_info:str, path_file_1: str, path_file_2: str, group_name: str):
+    def __init__(self, path_groups_info:str, path_folder: str, group_name: str):
         self.path_groups_info = path_groups_info
-        self.path_file_1 = path_file_1
-        self.path_file_2 = path_file_2
-        self.group_name = group_name
+        self.group_name = group_name        
+        self.df_groups_info = pd.read_csv(self.path_groups_info)
+        self.data_path = path_folder
+        
+        self.freq = self.__get_annotation_freq(self.df_groups_info, self.group_name)
+        if self.freq == 90:
+            self.filename_1 = "group.task engagement.helenrisack.annotation~"
+            self.filename_2 = "task engagement.group.carlosgonzalez.annotation~"
+        elif self.freq == 60:
+            self.filename_1 = "task engagement60Hz.group.helenrisack.annotation~"
+            self.filename_2 = "task engagement60Hz.group.carlosgonzalez.annotation~"
+        
+        self.path_file_1: str = f"{self.data_path}/{self.filename_1}"
+        self.path_file_2: str = f"{self.data_path}/{self.filename_2}"
+
         self.df_eng_1: pd.DataFrame = pd.read_csv(self.path_file_1, sep=";", names=self.col_names)
         self.df_eng_2 : pd.DataFrame = pd.read_csv(self.path_file_2, sep=";", names=self.col_names)
-        self.df_groups_info = pd.read_csv(self.path_groups_info)
 
-
-    def __avg_eng_files_TS_secs(self, df_1: pd.DataFrame, df_2: pd.DataFrame, freq: float) -> pd.DataFrame:
-        print(f"File 1 has {len(df_1)} lines and File 2 has {len(df_2)} lines")
+    def __avg_eng_files_TS_secs(self, df_1: pd.DataFrame, df_2: pd.DataFrame) -> tuple[pd.DataFrame, bool]:
+        print(f"{self.group_name}: File 1 has {len(df_1)} lines and File 2 has {len(df_2)} lines")
         if len(df_1) != len(df_2):
-            print("Engagement files are not of equal size!")
-            return pd.DataFrame()
+            print(f"{self.group_name}: Engagement files are not of equal size! Aborting processing")
+            return pd.DataFrame(), False
         # drop conf column and make sure both dataframes are numeric
         df_1.drop("conf", axis=1, inplace=True)
         df_1['task_eng'] = pd.to_numeric(df_1['task_eng'], errors='coerce')
@@ -43,9 +57,9 @@ class EngagementProcessor:
         df_merged = df_merged.fillna(0)
         df_avg: pd.DataFrame = pd.DataFrame(df_merged.mean(axis=1), columns=['task_eng'])
         # add column for seconds per frame
-        ts_secs: list[float] = [x * (1/freq) for x in range(len(df_avg))]
+        ts_secs: list[float] = [x * (1/self.freq) for x in range(len(df_avg))]
         df_avg['seconds'] = ts_secs
-        return df_avg
+        return df_avg, True
 
     def __get_start_end_interaction(self, df_groups_info: pd.DataFrame, group_name: str) -> tuple[float, float]:
         # get start and end of interaction
@@ -54,6 +68,11 @@ class EngagementProcessor:
         length = df_groups_info[col_mask]['duration_interaction'].values[0]
         end = start + length
         return  start, end
+    
+    def __get_annotation_freq(self, df_groups_info: pd.DataFrame, group_name: str) -> int:
+        col_mask: pd.Series[bool] = df_groups_info['name'] == group_name
+        freq: int = df_groups_info[col_mask]['freq_task_eng'].values[0]
+        return freq
 
     def __slice_interaction_time(self, df: pd.DataFrame, start: float, end: float) -> pd.DataFrame:
         # slice df to only get interaction time
@@ -62,21 +81,23 @@ class EngagementProcessor:
         df_return = df.loc[index_interaction_start.values[0]:index_interaction_end.values[0]]
         return df_return
 
-    def process_task_engagement(self, save_to_disk:bool) -> pd.DataFrame:
-        df_avg: pd.DataFrame = self.__avg_eng_files_TS_secs(self.df_eng_1, self.df_eng_2, freq=90)
-        interaction_start, interaction_end = self.__get_start_end_interaction(self.df_groups_info, group_name=group_name)
+    def process_task_engagement(self, save_to_disk:bool) -> tuple[pd.DataFrame, pd.DataFrame]:
+        df_avg, result = self.__avg_eng_files_TS_secs(self.df_eng_1, self.df_eng_2)
+        if not result:
+            return pd.DataFrame(), pd.DataFrame()
+        interaction_start, interaction_end = self.__get_start_end_interaction(self.df_groups_info, group_name=self.group_name)
         df_interaction = self.__slice_interaction_time(df_avg, interaction_start, interaction_end)
         if save_to_disk:
-            df_avg.to_csv(f"data/annotations/recording_{group_name}/task_engagement_avg_all.csv")
-            df_interaction.to_csv(f"data/annotations/recording_{group_name}/task_engagement_avg_interaction.csv")
-
-        return pd.DataFrame()
+            df_avg.to_csv(f"data/annotations/recording_{self.group_name}/task_engagement{self.freq}Hz_avg_all.csv")
+            df_interaction.to_csv(f"data/annotations/recording_{self.group_name}/task_engagement{self.freq}Hz_avg_interaction.csv")
+        return df_avg, df_interaction
 
 if __name__ == '__main__':
     path_file_1: str = "data/annotations/dyad_01/group.task engagement.helenrisack.annotation~"
     path_file_2: str = "data/annotations/dyad_01/task engagement.group.carlosgonzalez.annotation~"
     path_groups_info: str = "data/group_durations_all_commas.csv"
+    folder_path = "data/annotations/dyad_01/"
     group_name: str = "dyad_01"
 
-    processor: EngagementProcessor = EngagementProcessor(path_groups_info, path_file_1, path_file_2, group_name)
+    processor: EngagementProcessor = EngagementProcessor(path_groups_info=path_groups_info, path_folder=folder_path, group_name=group_name)
     processor.process_task_engagement(save_to_disk=True)
--- preprocessing/engagement/engagements_manager.py
+++ preprocessing/engagement/engagements_manager.py
@@ -3,12 +3,10 @@ import os
 
 class EngagementsManager:
 
-    engagements_list: list[EngagementProcessor]
+    engagements_list: list[EngagementProcessor] = []
     data_path: str = "data/annotations"
     path_groups_info: str = "data/group_durations_all_commas.csv"
-    filename_1: str = "group.task engagement.helenrisack.annotation~"
-    filename_2: str = "task engagement.group.carlosgonzalez.annotation~"
-    folders: list[str] 
+    folders: list[str]
 
     def __init__(self, save_to_disk: bool) -> None:
         self.load_engagements(save_to_disk=save_to_disk)
@@ -17,12 +15,12 @@ class EngagementsManager:
     def load_engagements(self, save_to_disk: bool):
         self.folders = os.listdir(self.data_path)
         for folder in self.folders:
-            file_1_path: str = f"{self.data_path}/{folder}/{self.filename_1}"
-            file_2_path: str = f"{self.data_path}/{folder}/{self.filename_2}"
-            eng_processor = EngagementProcessor(path_groups_info=self.path_groups_info, path_file_1=file_1_path, path_file_2=file_2_path, group_name=folder.replace("recording", ""))
+            folder_path: str = f"{self.data_path}/{folder}"
+            eng_processor = EngagementProcessor(path_groups_info=self.path_groups_info, path_folder=folder_path, group_name=folder.replace("recording_", ""))
             eng_processor.process_task_engagement(save_to_disk=save_to_disk)
+            self.engagements_list.append(eng_processor)
         pass
 
 if __name__ == "__main__":
-
+    mngr_aux: EngagementsManager = EngagementsManager(True)
     print("done")
