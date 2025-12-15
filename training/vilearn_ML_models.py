5d1a1a11911ed9df7e889ba1a35986555c04c303
Carlos Gonzalez Diaz <carlotes_247@hotmail.com>
2025-12-16 00:02:23 +0100

    Separated poly kernel from the rest in models to have less features to explore for it

--- training/vilearn_ML_models.py
+++ training/vilearn_ML_models.py
@@ -78,13 +78,22 @@ class VilearnMLModels:
                     'max_iter': [5000, 10000, 50000]
                 }
             },
-            "SVM": {
+            "SVM linear or rbf": {
                 'estimator': svm.SVC(),
                 'params': {
-                    'kernel': ['linear', 'rbf', 'poly'],
+                    'kernel': ['linear', 'rbf'],
                     'C': [0.1, 1],
                     'gamma': [0.1,0.01,0.001],
                     'degree':[0,1,2,4]
+                }
+            },
+            "SVM poly": {
+                'estimator': svm.SVC(),
+                'params': {
+                    'kernel': ['poly'],
+                    'C': [0.1, 1],
+                    'gamma': [0.1,0.01],
+                    'degree':[1,3]
                 }
             },
             "Decision Tree": {
