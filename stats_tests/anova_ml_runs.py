609f811d339aaa9a8c5a2573d5eb2ccd3bbf2ffe
Carlos Gonzalez Diaz <carlotes_247@hotmail.com>
2026-01-05 15:46:53 +0100

    Produced results ANOVA

--- stats_tests/anova_ml_runs.py
+++ stats_tests/anova_ml_runs.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import scipy.stats as stats
 import statsmodels.api as sm
 from statsmodels.formula.api import ols
+import statsmodels.stats.multicomp as mc
 import os
 
 if __name__ == "__main__":
@@ -14,7 +15,7 @@ if __name__ == "__main__":
     # files three way
     # simple, one file processed with relevant models from excel
     # TODO: do a run for all groups all features simple-scalar because we are missing baselines
-    file_three_way:str ="Processed_Results_Accuracy_three_way.csv"
+    file_three_way:str ="Processed_Results_Accuracy_three_way_60s.csv"
     file_path:str = os.path.join(os.getcwd(), data_folder, file_three_way)
     df_three_way: pd.DataFrame = pd.read_csv(file_path)
     df_three_way.drop(columns=['Time'], inplace=True)
@@ -28,34 +29,34 @@ if __name__ == "__main__":
     # also the baseline
     models_selected: list[str] = [
         "Baseline Most Frequent Strategy",
-        "Nearest Neighbors",
-        "Neural Net",
-        "Linear SVM l1",
-        "Decision Tree",
-        "Naive Bayes",
-        "QDA"]
+        "Nearest Neighbors", # beats baseline in triads aixvr scalar
+        "Neural Net", # beats baseline in triads aixvr simple
+        "Linear SVM l1", # beats baseline in triads & dyads all features
+        # "Decision Tree", 
+        # "Naive Bayes",
+        "QDA" # beats baseline in triads aixvr simple
+        ]
     # beat the baseline in simple version 3-way:
         # only triads aixvr (qda, neural net)
-        # unkown all groups, all aivr
     # beat the baseline in scaler version 3-way:
         # triads all (linear svm l1)
-        # triads aivr (knn)
+        # triads aixvr (knn)
         # dyads all (linear svm l1)
-        # unknown all groups, all aivr
 
     # discriminate for wanted models
     df_subset = df_three_way.loc[df_three_way['Model'].isin(models_selected)]
     # select simple
     df_simple = df_subset.loc[df_subset['Version'] == 'Simple_Manual']
-    df_simple.drop(columns=['Version'], inplace=True)
+    df_simple = df_simple.drop(columns=['Version'])
     # select scaler
     df_scaler = df_subset.loc[df_subset['Version'] == 'Scaler_Manual']
-    df_scaler.drop(columns=['Version'], inplace=True)
+    df_scaler = df_scaler.drop(columns=['Version'])
     print("DFs created")
 
     # perform the one-way anova on models
     # ANOVA: 5models [4models and the baseline] x 3dataset [dyads, triads, all] x  2 features [all, or AIxVR features]
-    # three way ANOVA with interactions across all conditions
+    
+    # # three way ANOVA with interactions across all conditions
     # formula = """
     # Score ~ C(Model) + C(Groups) + C(Features) +
     #         C(Model):C(Groups) +
@@ -63,11 +64,32 @@ if __name__ == "__main__":
     #         C(Groups):C(Features) +
     #         C(Model):C(Groups):C(Features)
     # """
+    formula_interactions_single = """
+    Score ~ C(Model) + C(Groups) + C(Features) + 
+    C(Model):C(Groups) + C(Model):C(Features) + C(Groups):C(Features)
+    """
     # simpler three way ANOVA without interactions
-    formula = "Score ~ C(Model) + C(Groups) + C(Features)"
+    formula_single = "Score ~ C(Model) + C(Groups) + C(Features)"
     
-    model = ols(formula, data=df_simple).fit()
+    # formula taking into account the version as one more factor
+    formula_version = "Score ~ C(Model) + C(Groups) + C(Features) + C(Version)"
+    formula_interactions_version = """
+    Score ~ C(Model) + C(Groups) + C(Features) + C(Version) + 
+    C(Model):C(Groups) + C(Model):C(Features) + C(Model):C(Version) 
+    + C(Groups):C(Features) + C(Groups):C(Version) + 
+    C(Features):C(Version)
+    """
+    df_anova = df_subset
+    formula = formula_version
+    model = ols(formula, data=df_anova).fit()
     anova_table = sm.stats.anova_lm(model, typ=2)  # or typ=3
     print(anova_table)
-    # posthoc test
+    anova_table.to_csv("anova_results.csv", index=True)
+    
+    # posthoc test (Groups was significant)
+    comp = mc.MultiComparison(df_anova["Score"], df_anova["Groups"])
+    posthoc_res = comp.tukeyhsd(alpha=0.05)
+    print(posthoc_res.summary())
+    tukey_df = posthoc_res.summary_frame()
+    tukey_df.to_csv("tukey_posthoc.csv", index=False)
     
