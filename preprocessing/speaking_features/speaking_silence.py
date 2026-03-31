7ad93dc633b325a596a99bdd44fa3effb8d60f8d
Carlos Gonzalez Diaz <carlotes_247@hotmail.com>
2026-03-31 17:13:41 +0200

    Upsampled all discover data recordings to 90Hz

--- preprocessing/speaking_features/speaking_silence.py
+++ preprocessing/speaking_features/speaking_silence.py
@@ -1,6 +1,9 @@
 import pandas as pd
 
 if __name__ == "__main__": 
+    # vars
+    upsample: bool = True
+    save_upsampled:bool = True
     floorlevel_info_pd = pd.read_csv("data/group_names_with_time_floorlevel.csv", sep=";")
     times_info_pd = pd.read_csv("data/group_durations_all_commas.csv", sep=",")
     group_names = floorlevel_info_pd["Group_Name"].to_list()
@@ -8,16 +11,45 @@ if __name__ == "__main__":
     for group_name in group_names:
         group_dis_pd = pd.read_csv(f"data/discover/merged/recording_{group_name}.csv")
         print(group_name)
-         # slice interaction time
+        # set dyad or triad participants
+        participants = ["speaking_p_blue", "speaking_p_green"] if "dyad" in group_name else ["speaking_p_blue", "speaking_p_green", "speaking_p_red"]
+        triad = True if "triad" in group_name else False  
+
+        if upsample:
+            # resample dataframe to 90 Hz
+            group_dis_pd['timestamp'] = pd.to_datetime(group_dis_pd['time_ms'], unit='ms')
+            group_dis_pd = group_dis_pd.set_index('timestamp')
+            # 11 ms expressed in pandas offset alias
+            freq = '11ms'          # "L" = millisecond, can also use "11ms"
+
+            # Create a new index that spans the whole range at 11 ms steps
+            new_index = pd.date_range(start=group_dis_pd.index.min(),
+                                    end=group_dis_pd.index.max(),
+                                    freq=freq)
+
+            # Reindex onto the new grid (introduces NaNs)
+            df_resampled = group_dis_pd.reindex(new_index)
+            df_resampled.index.name = 'timestamp'
+            df_merged = pd.merge_asof(df_resampled.sort_values('timestamp'), group_dis_pd.sort_values('timestamp'), on='timestamp', direction='nearest')
+            df_merged.drop(columns=[c for c in df_merged.columns
+                       if c.endswith('_x') and c != 'time_ms_x'], inplace=True)
+            # Linear interpolation (default for numeric columns)
+            df_interpolated = df_merged.interpolate(method='linear', limit_direction='forward', limit=None).ffill()
+            df_interpolated.drop(columns=['time_ms_y'], inplace=True)
+            df_interpolated.columns = df_interpolated.columns.str.replace(r'_[xy]$', '', regex=True)
+            # Reassign df for later processing
+            group_dis_pd = df_interpolated
+            if save_upsampled:
+                group_dis_pd.to_csv(f"data/discover/merged/recording_{group_name}_90Hz.csv")
+            
+
+        # slice interaction time
         inter_start_ms = times_info_pd[times_info_pd["name"]==group_name]["offset_recording_interaction_start"].iloc[0]*1000
         inter_length_ms = times_info_pd[times_info_pd["name"]==group_name]["duration_interaction"].iloc[0]*1000
         inter_end_ms = inter_start_ms+inter_length_ms
         first_inter_frame = (group_dis_pd['time_ms']-inter_start_ms).abs().argsort()[:1].iloc[0]
         last_inter_frame = (group_dis_pd['time_ms']-inter_end_ms).abs().argsort()[:1].iloc[0]
-        group_dis_pd = group_dis_pd[first_inter_frame:last_inter_frame]
-        # set dyad or triad participants
-        participants = ["speaking_p_blue", "speaking_p_green"] if "dyad" in group_name else ["speaking_p_blue", "speaking_p_green", "speaking_p_red"]
-        triad = True if "triad" in group_name else False        
+        group_dis_pd = group_dis_pd[first_inter_frame:last_inter_frame]              
 
         # Speaking configs
         length_df = len(group_dis_pd.index)
@@ -61,4 +93,6 @@ if __name__ == "__main__":
         speaking_configs_list.append(group_speaking_config_dict)
 
         print(group_dis_pd.size)
+    speaking_confis_pd = pd.DataFrame(speaking_configs_list)
+
     print("hola")
