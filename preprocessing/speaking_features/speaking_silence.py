94c82ec946f50bdb6407d962004686aaf7329318
Carlos Gonzalez Diaz <carlotes_247@hotmail.com>
2026-04-01 11:31:32 +0200

    Produced a single df with all speaking features

--- preprocessing/speaking_features/speaking_silence.py
+++ preprocessing/speaking_features/speaking_silence.py
@@ -4,12 +4,14 @@ if __name__ == "__main__":
     # vars
     upsample: bool = True
     save_upsampled:bool = True
-    floorlevel_info_pd = pd.read_csv("data/group_names_with_time_floorlevel.csv", sep=";")
-    times_info_pd = pd.read_csv("data/group_durations_all_commas.csv", sep=",")
-    group_names = floorlevel_info_pd["Group_Name"].to_list()
+    save_all_groups_df:bool = True
+    floorlevel_info_df = pd.read_csv("data/group_names_with_time_floorlevel.csv", sep=";")
+    times_info_df = pd.read_csv("data/group_durations_all_commas.csv", sep=",")
+    group_names = floorlevel_info_df["Group_Name"].to_list()
     speaking_configs_list = []
+    speaking_all_groups_df_list = []
     for group_name in group_names:
-        group_dis_pd = pd.read_csv(f"data/discover/merged/recording_{group_name}.csv")
+        group_dis_df = pd.read_csv(f"data/discover/merged/recording_{group_name}.csv")
         print(group_name)
         # set dyad or triad participants
         participants = ["speaking_p_blue", "speaking_p_green"] if "dyad" in group_name else ["speaking_p_blue", "speaking_p_green", "speaking_p_red"]
@@ -17,20 +19,20 @@ if __name__ == "__main__":
 
         if upsample:
             # resample dataframe to 90 Hz
-            group_dis_pd['timestamp'] = pd.to_datetime(group_dis_pd['time_ms'], unit='ms')
-            group_dis_pd = group_dis_pd.set_index('timestamp')
+            group_dis_df['timestamp'] = pd.to_datetime(group_dis_df['time_ms'], unit='ms')
+            group_dis_df = group_dis_df.set_index('timestamp')
             # 11 ms expressed in pandas offset alias
             freq = '11ms'          # "L" = millisecond, can also use "11ms"
 
             # Create a new index that spans the whole range at 11 ms steps
-            new_index = pd.date_range(start=group_dis_pd.index.min(),
-                                    end=group_dis_pd.index.max(),
+            new_index = pd.date_range(start=group_dis_df.index.min(),
+                                    end=group_dis_df.index.max(),
                                     freq=freq)
 
             # Reindex onto the new grid (introduces NaNs)
-            df_resampled = group_dis_pd.reindex(new_index)
+            df_resampled = group_dis_df.reindex(new_index)
             df_resampled.index.name = 'timestamp'
-            df_merged = pd.merge_asof(df_resampled.sort_values('timestamp'), group_dis_pd.sort_values('timestamp'), on='timestamp', direction='nearest')
+            df_merged = pd.merge_asof(df_resampled.sort_values('timestamp'), group_dis_df.sort_values('timestamp'), on='timestamp', direction='nearest')
             df_merged.drop(columns=[c for c in df_merged.columns
                        if c.endswith('_x') and c != 'time_ms_x'], inplace=True)
             # Linear interpolation (default for numeric columns)
@@ -38,46 +40,50 @@ if __name__ == "__main__":
             df_interpolated.drop(columns=['time_ms_y'], inplace=True)
             df_interpolated.columns = df_interpolated.columns.str.replace(r'_[xy]$', '', regex=True)
             # Reassign df for later processing
-            group_dis_pd = df_interpolated
+            group_dis_df = df_interpolated
             if save_upsampled:
-                group_dis_pd.to_csv(f"data/discover/merged/recording_{group_name}_90Hz.csv")
+                group_dis_df.to_csv(f"data/discover/merged/recording_{group_name}_90Hz.csv")
             
 
         # slice interaction time
-        inter_start_ms = times_info_pd[times_info_pd["name"]==group_name]["offset_recording_interaction_start"].iloc[0]*1000
-        inter_length_ms = times_info_pd[times_info_pd["name"]==group_name]["duration_interaction"].iloc[0]*1000
+        inter_start_ms = times_info_df[times_info_df["name"]==group_name]["offset_recording_interaction_start"].iloc[0]*1000
+        inter_length_ms = times_info_df[times_info_df["name"]==group_name]["duration_interaction"].iloc[0]*1000
         inter_end_ms = inter_start_ms+inter_length_ms
-        first_inter_frame = (group_dis_pd['time_ms']-inter_start_ms).abs().argsort()[:1].iloc[0]
-        last_inter_frame = (group_dis_pd['time_ms']-inter_end_ms).abs().argsort()[:1].iloc[0]
-        group_dis_pd = group_dis_pd[first_inter_frame:last_inter_frame]              
+        first_inter_frame = (group_dis_df['time_ms']-inter_start_ms).abs().argsort()[:1].iloc[0]
+        last_inter_frame = (group_dis_df['time_ms']-inter_end_ms).abs().argsort()[:1].iloc[0]
+        group_dis_df = pd.DataFrame(group_dis_df[first_inter_frame:last_inter_frame])              
 
         # Speaking configs
-        length_df = len(group_dis_pd.index)
+        length_df = len(group_dis_df.index)
         # Dyads
         # SP (One speaks)
-        SP_mask = group_dis_pd[participants].sum(axis=1) == 1
+        SP_mask = group_dis_df[participants].sum(axis=1) == 1
         SP_count = 0
         if True in SP_mask.value_counts():
             SP_count = SP_mask.value_counts()[True]         
         SP_amount = SP_count/length_df
+        group_dis_df = group_dis_df.assign(SP=SP_mask.values) # assign to group df
         # SI (All silent (~ symbol negates the dataframe))
-        SI_mask = ~group_dis_pd[participants].any(axis=1)
+        SI_mask = ~group_dis_df[participants].any(axis=1)
         SI_count = 0
         if True in SI_mask.value_counts():
             SI_count = SI_mask.value_counts()[True]
         SI_amount = SI_count/length_df
+        group_dis_df = group_dis_df.assign(SI=SI_mask.values)
         # OV (Overlap)
         # All speak
-        OV_all_speak_mask = group_dis_pd[participants].all(axis=1)
+        OV_all_speak_mask = group_dis_df[participants].all(axis=1)
         OV_all_speak_count = 0
         if True in OV_all_speak_mask.value_counts():
             OV_all_speak_count = OV_all_speak_mask.value_counts()[True]  
         OV_all_speak_amount = OV_all_speak_count /length_df
+        group_dis_df = group_dis_df.assign(OV_all_SP=OV_all_speak_mask.values)
         # Two speak (only triads)
         OV_two_speak_count = 0
         OV_two_speak_amount = 0
+        OV_two_speak_mask = group_dis_df[participants].sum(axis=1) == 2
+        group_dis_df = group_dis_df.assign(OV_two_SP=OV_two_speak_mask.values)
         if triad:
-            OV_two_speak_mask = group_dis_pd[participants].sum(axis=1) == 2
             if True in OV_two_speak_mask.value_counts():
                 OV_two_speak_count = OV_two_speak_mask.value_counts()[True]
                 OV_two_speak_amount = OV_two_speak_count /length_df
@@ -91,8 +97,13 @@ if __name__ == "__main__":
             "OV_two_sp": OV_two_speak_amount*100
         }
         speaking_configs_list.append(group_speaking_config_dict)
+        speaking_all_groups_df_list.append(group_dis_df)
 
-        print(group_dis_pd.size)
+        print(group_dis_df.size)
     speaking_confis_pd = pd.DataFrame(speaking_configs_list)
+    speaking_all_groups_df = pd.concat(speaking_all_groups_df_list, ignore_index=True)
+    if save_all_groups_df:
+        speaking_all_groups_df.to_csv("data/discover/merged/all_groups_interaction_speaking_90Hz.csv")
+    # all_groups_interaction_task_eng90Hz
 
     print("hola")
