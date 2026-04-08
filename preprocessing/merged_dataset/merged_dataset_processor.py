8382010816039ea032ddf742475ae0ede7970733
Carlos Gonzalez Diaz <carlotes_247@hotmail.com>
2026-04-08 13:50:51 +0200

    Prepared an skeleton to merge the windows of speaking silence with the rest of the data

--- preprocessing/merged_dataset/merged_dataset_processor.py
+++ preprocessing/merged_dataset/merged_dataset_processor.py
@@ -2,27 +2,32 @@ import pandas as pd
 import os
 import datetime
 
-# TO USE THIS FILE YOU NEED TO PRODUCE FOUR DIFFERENT FILES
+# TO USE THIS FILE YOU NEED TO PRODUCE SEVERAL DIFFERENT FILES
 # 1. gaze file from plotting/plotting_eye_data.py by calling eye_plotter.create_line_plot(sampling)
 # 2. blink durations file from data_reading/features/blink_stats.py by calling calculate_blink_duration_for_each_minute(sampling), this is called as part of another method in line 220
 # 3. blink rate file from data_reading/features/blink_stats.py by calling calculate_blink_rate(sampling) which is called as part of class_obj.get_groups_blink_rate(sampling)
 # 4: task engagement (TE) file from engagement_manager, but we can use the 90Hz file in data/annotations. It is not resampled to a particular sample rate, this is done in this processor class
+# 5: speaking-silence file from discover server
 class MergedDatasetProcessor():
     df_processed: pd.DataFrame
     df_wide_gaze: pd.DataFrame
     df_long_gaze: pd.DataFrame
+    df_speaking: pd.DataFrame
+    df_TE: pd.DataFrame
     working_dir: str
     data_folder_gaze: str 
     data_folder_blinks: str
     data_folder_TE: str
+    data_folder_speaking: str
     file_path_gaze: str
     file_path_blink_duration_dyads:str
     file_path_blink_duration_triads:str
     file_path_blink_rate:str
     file_path_TE:str
+    file_path_speaking: str
     group_names_file:str 
 
-    def __init__(self, sampling:int, filename_gaze:str, filename_blink_duration_dyads:str, filename_blink_duration_triads:str, filename_blink_rate:str, filename_TE:str, data_folder_gaze:str = "", data_folder_blinks:str = "", data_folder_TE:str = ""):
+    def __init__(self, sampling:int, filename_gaze:str, filename_blink_duration_dyads:str, filename_blink_duration_triads:str, filename_blink_rate:str, filename_speaking:str, filename_TE:str, data_folder_gaze:str = "", data_folder_blinks:str = "", data_folder_TE:str = ""):
         # load dataframes
         self.working_dir = os.getcwd()
         # gaze (wide df)
@@ -40,12 +45,17 @@ class MergedDatasetProcessor():
         full_path_blink_rates = os.path.join(self.working_dir, self.data_folder_blinks, self.file_path_blink_rate)
         self.df_blink_duration_dyads = pd.read_csv(full_path_blink_duration_dyads)
         self.df_blink_duration_triads = pd.read_csv(full_path_blink_duration_triads)
-        self.df_blink_rates = pd.read_csv(full_path_blink_rates)        
+        self.df_blink_rates = pd.read_csv(full_path_blink_rates)    
+        # Speaking
+        self.data_folder_speaking = 'data/discover/merged'
+        self.file_path_speaking = filename_speaking
+        full_path_speaking = os.path.join(self.working_dir, self.data_folder_speaking, self.file_path_speaking)  
+        self.df_speaking = pd.read_csv(full_path_speaking)     
         # TE (not sampled)
         self.data_folder_TE = 'data/annotations/' if data_folder_TE == "" else data_folder_TE
         self.file_path_TE = "all_groups_interaction_task_eng90Hz.csv" if filename_TE == "" else filename_TE
         full_path_TE = os.path.join(self.working_dir, self.data_folder_TE, self.file_path_TE)        
-        self.df_TE = pd.read_csv(full_path_TE)
+        self.df_TE = pd.read_csv(full_path_TE)     
         # group_names
         group_names_df = pd.read_csv(os.path.join(self.working_dir, 'data', 'group_names_with_time_subsetsFullVERSION.csv'), sep=';')
         group_names = group_names_df['Group_Name'].to_list()
@@ -57,6 +67,8 @@ class MergedDatasetProcessor():
         # merge blink durations into dataset
         df_merged = self.__merge_df_blink_durations_into_df_long_gaze(self.df_blink_duration_dyads, self.df_blink_duration_triads,
                                                                     df_merged, group_names)
+        # merge speaking into dataset
+        self.__merge_df_speaking_into_df_long_gaze(self.df_speaking, df_merged, group_names)
         # add TE column
         df_merged = self.__merge_resample_df_TE_into_df_long_gaze(sampling, self.df_TE, df_merged, group_names)
         # reorder columns to match the original 60s file that Cristina made by hand
@@ -131,6 +143,16 @@ class MergedDatasetProcessor():
                     
         return df_merged_all.drop(columns=['group_name_y', 'TSGroupNTP', 'P1_durations', 'P2_durations', 'P3_durations']).rename(columns={'group_name_x':'group_name', 'group_avg_blink_duration':'blink_durations'})
 
+    def __merge_df_speaking_into_df_long_gaze(self, df_speaking: pd.DataFrame, df_long_gaze: pd.DataFrame, group_names: list[str]) -> pd.DataFrame:
+        # merge gaze and speaking per group
+        for group_name in group_names:
+            df_group_speaking = df_speaking[df_speaking["session"] == group_name]
+            df_group_gaze = df_long_gaze[df_long_gaze['group_name'] == group_name]
+            # merge gaze and speaking per group
+            
+            print("hola")
+        pass
+
     def __merge_resample_df_TE_into_df_long_gaze(self, sampling:int, df_TE:pd.DataFrame, df_long_gaze:pd.DataFrame, group_names:list[str]) -> pd.DataFrame:
         df_TE['TS_ms'] = pd.to_timedelta(df_TE['seconds'], unit='s')
         df_TE = df_TE.set_index('TS_ms')
@@ -156,12 +178,13 @@ class MergedDatasetProcessor():
         self.df_processed.to_csv(os.path.join(self.data_folder_gaze, filename))
 
 if __name__ == "__main__":
-    process_30s: bool = True
-    sampling: int = 30
+    process_30s: bool = False
+    sampling: int = 60
     file_name_gaze:str = ""
     file_name_blink_duration_dyads:str = ""
     file_name_blink_duration_triads:str = ""
     file_name_blink_rate:str =""
+    file_name_speaking:str = ""
     file_name_TE:str = ""
     # 30s
     if process_30s and sampling == 30:
@@ -169,16 +192,22 @@ if __name__ == "__main__":
         file_name_blink_duration_dyads = f"blink_duration_per_minute_dyads_{sampling}s_2025-12-22.csv"
         file_name_blink_duration_triads = f"blink_duration_per_minute_triads_{sampling}s_2025-12-22.csv"
         file_name_blink_rate = f"blink_per_minute_all_groups_{sampling}s_2025-12-22.csv"
+        file_name_speaking = f"all_groups_interaction_speaking_per_{sampling}s.csv"
         file_name_TE = "all_groups_interaction_task_eng90Hz.csv"
     # 60s
     else:
         file_name_gaze = 'all_features_60s_resampled.csv'
+        file_name_blink_duration_dyads = f"blink_duration_per_minute_dyads_{sampling}s.csv"
+        file_name_blink_duration_triads = f"blink_duration_per_minute_triads_{sampling}s.csv"
+        file_name_blink_rate = f"blink_per_minute_all_groups.csv"
+        file_name_speaking = f"all_groups_interaction_speaking_per_{sampling}s.csv"
         # data_file_with_TE = '60s_TE_correlation.csv'
 
     datasetCtrl: MergedDatasetProcessor = MergedDatasetProcessor(sampling=sampling, filename_gaze=file_name_gaze,
                                                                  filename_blink_duration_dyads=file_name_blink_duration_dyads, 
                                                                  filename_blink_duration_triads=file_name_blink_duration_triads,
                                                                  filename_blink_rate=file_name_blink_rate,
+                                                                 filename_speaking=file_name_speaking,
                                                                  filename_TE=file_name_TE)
 
     datasetCtrl.to_csv(f"{sampling}s_TE_correlation_{datetime.datetime.now().date()}.csv")
